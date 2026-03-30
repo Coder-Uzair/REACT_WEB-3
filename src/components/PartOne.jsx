f6090482e651f047423305c837cc66d71cbe4bdd
Coder-Uzair <githubuzair34@gmail.com>
2026-03-30 12:34:51 -0700

    Add files via upload

---
+++ src/components/PartOne.jsx
@@ -0,0 +1,8 @@
+export default function PartOne() {
+    return(
+    <>    
+    4(MgCO<sub>3</sub>) . Mg(OH)<sub>2</sub> . 4H<sub>2</sub>O
+    <br/>
+    </>
+    ); 
+}
---
+++ src/components/PartTwo.jsx
@@ -0,0 +1,8 @@
+export default function PartTwo() {
+    return(
+        <>
+        MgCO<sub>3</sub> . 3H<sub>2</sub>O
+        <br/>
+        </>
+    );
+}
